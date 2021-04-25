79a5f451f269fac0961040ab23416d390906e5c8
katratzi <ric_clifford@hotmail.com>
2021-04-25 10:15:42 +0100

    flutter - micard layout

---
+++ flutter_micard/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.teal,
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          color: Colors.yellow,
+                        ),
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          color: Colors.green,
+                        ),
+                      ]),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
