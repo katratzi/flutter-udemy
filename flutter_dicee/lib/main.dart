acc8acb7f92e60298d38f396be2697006888fcb5
katratzi <ric_clifford@hotmail.com>
2021-04-25 14:32:48 +0100

    flutter dicee app

---
+++ flutter_dicee/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // in stateful.  values sit outside build context
+  var leftDice = 4;
+  var rightDice = 3;
+
+// do ya feel lucky
+  void rollDice() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  rollDice();
+                },
+                child: Image.asset('images/dice$leftDice.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  rollDice();
+                },
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
